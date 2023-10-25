b2c6c4e2a84d29d87d0c3b29ec587336345dac5c
Simone Ramundi <simone.ramundi@4science.com>
2023-10-25 15:47:20 +0200

    Seeing the Requests Response

--- wsearch/src/app/app.component.ts
+++ wsearch/src/app/app.component.ts
@@ -7,10 +7,15 @@ import {WikipediaService} from "./wikipedia.service";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  pages = [];
   constructor(private wikipedia: WikipediaService) {}
   onTerm(term:string){
     // console.log(`I am the app and here is the term`, term)
-    const results = this.wikipedia.search(term);
-    console.log(results);
+    // const results = this.wikipedia.search(term);
+    // console.log(results);
+    this.wikipedia.search(term).subscribe((response:any)=> {
+      // console.log(response);
+      this.pages = response.query.search;
+    });
   }
 }
